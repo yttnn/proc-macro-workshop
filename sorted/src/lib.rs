156ae503f9f67cb9cb8fd6ee630847dbbd6f3cf3
yttnn <67893139+yttnn@users.noreply.github.com>
2023-11-01 18:07:42 +0900

    01 parse-enum

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -1,9 +1,14 @@
 use proc_macro::TokenStream;
+use syn::{parse_macro_input, Item};
 
 #[proc_macro_attribute]
 pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
     let _ = args;
     let _ = input;
 
-    unimplemented!()
+    eprintln!("args : {:#?}", args);
+    eprintln!("input: {:#?}", input);
+    let _item = parse_macro_input!(input as Item);
+    eprintln!("item : {:#?}", _item);
+    TokenStream::new()
 }
